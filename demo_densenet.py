28b604736faa3e2eda4ae03bb50c22c37226e3c0
Adolfo Gonzalez III <adolfo.gonzalez02@utrgv.edu>
2019-05-07 21:25:42 -0500

    Added demos for each model Had to add ShufflenetV2 as well since its currently missing from the conda torch package.

---
+++ demo_densenet.py
@@ -0,0 +1,35 @@
+import torch
+import pathlib
+
+from nets import create_densenet, create_squeezenet, create_shufflenet
+
+from demo_utils import demo
+
+def load_model(model_name, nInputChannels):
+    if model_name == 'densenet':
+        return create_densenet(nInputChannels)
+    elif model_name == 'squeezenet':
+        return create_squeezenet(nInputChannels)
+    elif model_name == 'shufflenet':
+        return create_shufflenet(nInputChannels)
+
+def main():
+    model_name = 'densenet'
+    gpu_id = 0
+    device = torch.device("cpu")
+    #device = torch.device("cuda:" + str(gpu_id))
+
+    #  Create the network and load the weights
+    net = load_model(model_name, 4)
+    project_dir = pathlib.Path(__file__).resolve().parent
+    weights_path = project_dir / 'models' / '{}.pth'.format(model_name)
+    net_parameters = torch.load(str(weights_path), map_location=lambda s, l: s)
+    net.load_state_dict(net_parameters)
+    net.eval()
+    net.to(device)
+    with torch.no_grad():
+        demo(net)
+
+
+if __name__ == '__main__':
+    main()
